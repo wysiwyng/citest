50f3b48a7c7e5c2d59c1f72bafdac3a8322034f1
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2021-11-26 15:27:58 +0100

    changes

--- local.py
+++ local.py
@@ -16,7 +16,7 @@ shutil.move('new/run_results.json', 'old/run_results.json')
 
 with open('run_results.json', 'w') as f:
     json.dump({'mips': int(sys.argv[1])}, f)
-
     
 shutil.move('run_results.json', 'new')
 compare.main('new/run_results.json', 'old/run_results.json', make_hash(), 0.1, False)
+
